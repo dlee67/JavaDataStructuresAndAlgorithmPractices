122554c7932e01acbec000ef9c84563e759d113e
bob <dlee67@msudenver.edu>
2017-12-26 13:59:00 -0700

    Wow, much fun, such shiny, very ruby.

---
+++ RubyPracs/addTwo.rb
@@ -0,0 +1,15 @@
+# Ruby doesn't require a return keyword for the value to be returned.
+
+def addTwo(x)
+	x + 2
+end
+
+y = addTwo(2)
+print(y, "\n")
+
+def addThree(y)
+	q = y + 3
+end
+
+z = addThree(2)
+print(z, "\n")
