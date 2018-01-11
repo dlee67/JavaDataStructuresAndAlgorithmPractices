919126a6e5a08d070475a4f5c24b5bdfe231d2a3
bob <dlee67@msudenver.edu>
2018-01-11 16:10:30 -0700

    Prac

--- OctavePrac/script41.m
+++ OctavePrac/script41.m
@@ -1,11 +1,1 @@
-a = input("Chuck in a number: ")
-
-if (a < 5)
-  disp("Your input is less than 5")
-elseif (a > 5)
-  disp("The input is bigger than 5")
-elseif (a == 5)
-  disp("Your input is 5")
-else
-  disp("Is the input a number?")
-endif
+wakeup ("Rise and shine!");
---
+++ OctavePrac/wakeup.m
@@ -0,0 +1,4 @@
+%So, function behaves like a class from Java?
+function wakeup (message)
+	disp(message)
+endfunction
