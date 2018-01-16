74a4131403e6b83e837695bc1424a86d0ef4c096
bob <dlee67@msudenver.edu>
2018-01-16 11:52:43 -0700

    Prac

--- JSPrac/salHasExer.js
+++ JSPrac/salHasExer.js
@@ -1,20 +1,21 @@
-compareString="abc"
-compareWithString="cba"
+compareString="abcad"
+compareWithString="cbada"
 function isPermu(compareThis, compareWith){
-	//So, compare if all my characters in the compareWith is in the compareThis list?
-	//When a matching characters are found for both of the list, take'em out from
-	//both the lists.
-	for(var forCT=0;forCT < compareThis.length(); forCT++){
-		for(var forCW=0;forCW < compareWith.length(); forCW++){
-			if((compareThis.charAt(forCT) && compareWith.charAt(forCW)) == null){
-				return true;
-			}
+	console.log("Compare this now: " + compareThis + "\n");
+	console.log("Compare with now: " + compareWith + "\n");
+	if(compareThis==''){
+		if(compareWith==''){
+			return "The sequence of characters are permutable to each other.";
+		}
+	}
+	for(var forCT=0;forCT < compareThis.length; forCT++){
+		for(var forCW=0;forCW < compareWith.length; forCW++){
 			if(compareThis.charAt(forCT) == compareWith.charAt(forCW)){
-				isPermu(compareThis.replace(compareThis.charAt(forCt)), 
-						compareWith.replace(compareWith.charAt(forCW)));
-			}
+				return isPermu(compareThis.replace(compareThis.charAt(forCT), ''), 
+						compareWith.replace(compareWith.charAt(forCW), ''));
+			}			
 		}
 	}	
+		return "The sequences of characters are not permutable to each other."
 }
-
-console.log(isPermu(compareString, compareWithString);
+console.log(isPermu(compareString, compareWithString));
