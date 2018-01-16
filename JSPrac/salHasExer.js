34d49199d6e8b6c6b8f837eef730143a40b88bcb
bob <dlee67@msudenver.edu>
2018-01-16 08:54:53 -0700

    Prac

--- JSPrac/salHasExer.js
+++ JSPrac/salHasExer.js
@@ -1,3 +1,15 @@
+compareString="abc"
+compareWithString="cba"
 function isPermu(compareThis, compareWith){
 	//So, compare if all my characters in the compareWith is in the compareThis list?
+	//When a matching characters are found for both of the list, take'em out from
+	//both the lists.
+	for(var forCT=0;forCT < compareThis.length(); forCT++){
+		for(var forCW=0;forCW < compareWith.length(); forCW++){
+			if(compareThis.charAt(forCT) == compareWith.charAt(forCW)){
+				isPermu(compareThis.replace(compareThis.charAt(forCt)), 
+						compareWith.replace(compareWith.charAt(forCW)));
+			}
+		}
+	}
 }
