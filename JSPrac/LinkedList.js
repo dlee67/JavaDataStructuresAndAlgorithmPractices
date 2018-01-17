0d5ff259decfe54724c28a7c985745b347f04e9a
bob <dlee67@msudenver.edu>
2018-01-16 18:15:04 -0700

    Prac

--- JSPrac/LinkedList.js
+++ JSPrac/LinkedList.js
@@ -8,10 +8,28 @@ function NodePrototype(someNode, someVal){
 var LinkedList={
 	header: null,
 	addNode: function(someVal){
+		//console.log("In addNode");
 		if(this.header == null){
 			this.header = new NodePrototype(null, someVal);
+			//console.log("Has header: " + this.header.someVal);
 		}
 		var newNode = new NodePrototype(this.header.nextNode, someVal);
-		this.header = newNode;
+		this.header.nextNode = newNode;
+		//console.log("Has header: " + this.header.someVal);
+		//console.log("Header next's: " + this.header.nextNode);
+	},
+	printNodes: function(someNode){
+		var recurNode = someNode;
+		if(recurNode.nextNode != null){
+			console.log(recurNode.someVal);
+			this.printNodes(recurNode.nextNode);
+		}
 	}
 };
+
+LinkedList.addNode(1);
+LinkedList.addNode(2);
+LinkedList.addNode(3);
+LinkedList.addNode(4);
+LinkedList.addNode(5);
+LinkedList.printNodes(LinkedList.header);
