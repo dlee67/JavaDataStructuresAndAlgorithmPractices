f025e742c603576b9c8b803181e7af536e14ae5d
bob <dlee67@msudenver.edu>
2018-01-20 08:37:20 -0700

    Nice sever, my server.

--- JSPrac/MyServer.js
+++ JSPrac/MyServer.js
@@ -1,8 +1,11 @@
 var http = require('http');
-var server = http.createServer((req, res) => {
-	res.writeHead(200, {
-		'Contend-Type':'text/plain'
-	});
-	res.end("WOW! MY SERVER!");
-});
-server.listen(8080, '127.0.0.1');
+var server = http.createServer((req, res) => {  //  
+	res.writeHead(200, {                        //
+		'Contend-Type':'text/plain'             // In this particular exercise, I am skipping the statusMessage (which is optional),
+	});                                         // where the headers are the second arguments.
+	res.end("WOW! MY SERVER!");                 //
+});                                             //
+server.listen(8080);				// Pretty much the portion where I host a server on	a 
+												// IP 127.0.0.1: with the port 8080.
+console.log("I have a port opened in my localhost:8080"); 												
+								
