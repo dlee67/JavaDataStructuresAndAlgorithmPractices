75494eba99f93f7139bccfa7a8a3d65aa38ca9cf
bob <dlee67@msudenver.edu>
2018-01-30 20:54:53 -0700

    Prac

---
+++ JSPrac/test.js
@@ -0,0 +1,7 @@
+//Example from the Full Stack JavaScript Development with MEAN.
+
+var m = require('./mmm')
+
+console.log(m.add(3, 5));
+console.log(m.multiply(4, 5));
+console.log(m.factorial(4));
