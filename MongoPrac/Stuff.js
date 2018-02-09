27a41edce270872d7a607fdfbaed75364e0a4cd1
bob <dlee67@msudenver.edu>
2018-02-08 17:28:18 -0700

    Prac

--- MongoPrac/Stuff.js
+++ MongoPrac/Stuff.js
@@ -1,1 +1,14 @@
-var mongoose = require('mongoose');
+var MongoClient = require('mongodb').MongoClient;
+//The below is one way to connect to the mongodb database.
+var url = 'mongodb://127.0.0.1:27017';
+var db;
+
+MongoClient.connect(url, (err, db) => {
+    if (err) return cb(err);
+    console.log("Connected correctly to server");
+    console.log(db.listCollections());
+	db.collection("foo", (err, colle) => {
+		//console.log(colle.find());
+	});
+	db.close();
+});
