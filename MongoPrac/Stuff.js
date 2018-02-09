da1a329598c8db3eb049ee07ceb56ec3c5926da8
bob <dlee67@msudenver.edu>
2018-02-09 11:43:24 -0700

    Prac

--- MongoPrac/Stuff.js
+++ MongoPrac/Stuff.js
@@ -3,12 +3,27 @@ var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://127.0.0.1:27017';
 var db;
 
+var album = { _id: "112",
+
+              name: "italy2012",
+
+              title: "Spring Festival in Italy",
+
+              date: "2012/02/15",
+
+              description: "I went to Italy for Spring Festival." };
+
+//Appearantly, this creates the collection,
 MongoClient.connect(url, (err, db) => {
-    if (err) return cb(err);
-    console.log("Connected correctly to server");
-    console.log(db.listCollections());
-	db.collection("foo", (err, colle) => {
-		//console.log(colle.find());
+    if (err) throw err;
+	//Where the name of the collection, in this is "albums"
+    db.collection("albums", (err, aa) =>{
+		if (err) throw err;
+		aa.insertOne(album, (err, inserted_doc) => {
+			if(err) throw err;
+			console.log(inserted_doc);
+		});
 	});
 	db.close();
 });
+
