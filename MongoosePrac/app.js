35d1b62ab3405c51cc128b17d14f28a89d8aa4a2
bob <dlee67@msudenver.edu>
2018-02-11 22:45:03 -0700

    Prac

---
+++ MongoosePrac/app.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+var pug = require("pug");
+
+var http = require("http");
+
+
+//mongoose.connect("mongodb://localhost:8080/prac");
+
+//var schema = mongoose.Schema({
+//	value: {
+//		type: String
+//	}
+//});
+
+//module.exports = mongoose.model('schma', schema);	
+
+//Why?
+//Because app.listen allows the server to listen for port 8080, and that's about it.
+//I need make an actual http request for anything to happen on the browser.
+app.get("/", (req, res, next) => {
+	res.end("Super Saiyan 5");
+});
+app.listen(8080);
+
+//app.get("/", (req, res) => {
+//	res.end("Super Saiyan 5!");
+//});
+
