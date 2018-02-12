14fc7f0fdaa1ea2e6e139a0493a61a5ff7f569d7
bob <dlee67@msudenver.edu>
2018-02-11 17:53:54 -0700

    Prac

--- MeanAgain/app.js
+++ MeanAgain/app.js
@@ -3,9 +3,10 @@ const app = express();
 const path = require("path");
 
 //The line directly below solved the, "pug module not found problem.
-app.engine("pug", require("pug").__express);
+//Source: https://stackoverflow.com/questions/45342307/error-cannot-find-module-pug
+app.engine("html", require("ejs").__express);
 app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
 	let articles = [
@@ -28,14 +29,14 @@ app.get("/", (req, res) => {
 			body: "This is article three"
 		}
 	];
-	res.render("index.pug", {
+	res.render("index.html", {
 		title:"hello",
 		articles: articles
 	});
 });
 
 app.get('/articles/add', (req, res) => {
-	res.render("add_article.pug", {
+	res.render("add_article.html", {
 		title:"Add Article"
 	});
 });
