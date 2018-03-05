d0cce37b87482287913547de277d52f3bf6664c4
BOb <dlee67@msudenver.edu>
2018-03-05 13:30:27 -0700

    The usual.

--- OpenCVPrac/CannyEdge.cc
+++ OpenCVPrac/CannyEdge.cc
@@ -2,7 +2,7 @@
 
 int main( int argc, char** argv ) {
 
-    cv::Mat img_rgb, img_gry, img_cny;
+    cv::Mat img_rgb, img_gry, img_cny, img_pyr2;
 
     cv::namedWindow( "Example Gray",  cv::WINDOW_AUTOSIZE );
     cv::namedWindow( "Example Canny", cv::WINDOW_AUTOSIZE );
@@ -16,6 +16,29 @@ int main( int argc, char** argv ) {
     cv::Canny( img_gry, img_cny, 10, 100, 3, true );
     cv::imshow( "Example Canny", img_cny );
 
-    cv::waitKey(0);
+	 int x = 16, y = 32;
+	cv::Vec3b intensity = img_rgb.at< cv::Vec3b >(y, x);
 
+	// ( Note: We could write img_rgb.at< cv::Vec3b >(y,x)[0] )
+	//
+	uchar blue  = intensity[0];
+	uchar green = intensity[1];
+	uchar red   = intensity[2];
+
+	std::cout << "At (x,y) = (" << x << ", " << y <<
+          "): (blue, green, red) = (" <<
+          (unsigned int)blue <<
+          ", " << (unsigned int)green << ", " <<
+          (unsigned int)red << ")" << std::endl;
+
+	std::cout << "Gray pixel there is: " <<
+             (unsigned int)img_gry.at<uchar>(y, x) << std::endl;
+
+	x /= 4; y /= 4;
+	std::cout << "Pyramid2 pixel there is: " <<
+             (unsigned int)img_pyr2.at<uchar>(y, x) << std::endl;
+
+	img_cny.at<uchar>(x, y) = 128; // Set the Canny pixel there to 128
+
+    cv::waitKey(0);
 }
