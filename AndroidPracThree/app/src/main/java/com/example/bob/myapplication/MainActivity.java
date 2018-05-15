355dc21bd59c2a048bd2f3748ca95d33ffbddb16
dlee67 <dlee67@msudenver.edu>
2018-05-14 20:11:05 -0600

    The usual.

--- AndroidPracThree/app/src/main/java/com/example/bob/myapplication/MainActivity.java
+++ AndroidPracThree/app/src/main/java/com/example/bob/myapplication/MainActivity.java
@@ -5,12 +5,15 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     FragmentManager fragmentManager = getSupportFragmentManager();
     FragmentTransaction fT;
+    Button mutateButton;
     int cloneCount = 1;
 
     protected void addFragment(Fragment newFragment){
@@ -22,10 +25,25 @@ public class MainActivity extends AppCompatActivity {
         fT.commit();
     }
 
+    protected void mutateCloneFive(){
+        Fragment frag = fragmentManager.findFragmentByTag("clone5");
+        TextView mutateThis = frag.getView() // fetches the parent view, which the view that has been inflated via Fragment is a child of.
+                              .findViewById(R.id.fragmentTextView); // Since, findViewById() is a method that is part of View,
+                                                                    // I am allowed to do this.
+        mutateThis.setText("Mastered Ultra Instinct");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mutateButton = findViewById(R.id.mutateStuff);
+        mutateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mutateCloneFive();
+            }
+        });
         Fragment newFragment = new FragmentOne();
         fT = fragmentManager.beginTransaction();
         fT.add(R.id.stuffOne, newFragment, "originFrag");
