1518923fbd0d7998066fb2fbc62799b07adc31f4
dlee67 <dlee67@msudenver.edu>
2018-05-16 16:12:15 -0600

    The usual.

--- AndroidPracFour/app/src/main/java/com/example/bob/myapplication/FirstActivity.java
+++ AndroidPracFour/app/src/main/java/com/example/bob/myapplication/FirstActivity.java
@@ -1,9 +1,25 @@
 package com.example.bob.myapplication;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstActivity extends AppCompatActivity{
     //https://stackoverflow.com/questions/10477375/what-does-classname-class-return
-    Intent intent = new Intent(this, SecondActivity.class);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_screen);
+        Button button = findViewById(R.id.firstShoot);
+        final Intent newIntent = new Intent(this, SecondActivity.class);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(newIntent);
+                finish();
+            }
+        });
+    }
 }
--- AndroidPracFour/app/src/main/java/com/example/bob/myapplication/SecondActivity.java
+++ AndroidPracFour/app/src/main/java/com/example/bob/myapplication/SecondActivity.java
@@ -1,6 +1,23 @@
 package com.example.bob.myapplication;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_screen);
+        Button button = findViewById(R.id.shootSecond);
+        final Intent intent = new Intent(this, FirstActivity.class);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
 }
